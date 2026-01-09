2e2284b537bc25dc79668b18550398ad29781158
Yash Adke <122884838+yashgithub2003@users.noreply.github.com>
2026-01-09 14:01:01 +0000

    Arrays

---
+++ basics/03.js
@@ -0,0 +1,64 @@
+//  Array in JavaScript
+// const arr = [1,2,3,4]
+// const arr2 = new Array("A","E","I");
+// console.log(arr[3]);
+// console.log(arr2[2]);
+ 
+// Basic methods in Array
+// arr.push("Yash")
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.unshift(21) // add element at start of array
+// console.log(arr);
+// arr.shift()
+// console.log(arr); // remove first element
+
+// console.log(arr.includes(2));
+// console.log(arr.indexOf(2));
+
+// const newArr = arr.join()
+// console.log(typeof arr);
+// console.log(typeof newArr);
+
+// Slice and splice
+
+// let arr = [12,32,4,2,4]
+// console.log(arr);
+// let sliceArr = arr.slice(1,3)
+// console.log(arr);
+// console.log(sliceArr);
+// let spliceArr = arr.splice(1,3) // remove the slice element from original array
+// console.log(spliceArr);
+// console.log(arr);
+
+const a = [1,2,3,4,5]
+const b = [6,7,8,9]
+// console.log(a.push(b));
+// console.log(a.concat(b));
+
+// const c = [ ...a,...b]
+// console.log(c);
+
+// const z = [1,2,3,[4,5],6,[7]]
+// const x1 = z.flat(1)
+// console.log(x1);
+// const x2 = z.flat(8)
+// console.log(x2);
+// const x3 = z.flat(Infinity)
+// console.log(x3);
+
+
+// console.log(Array.isArray("YASH"));
+// console.log(Array.from("YASH"));
+// console.log(Array.from({name : "YASH"}));
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3));
+
+
+
+// ------------------- Objects ------------------
+
