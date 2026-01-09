6ae1e35c5c31065d1044b23493718c45ece49a48
Yash Adke <122884838+yashgithub2003@users.noreply.github.com>
2026-01-09 07:08:33 +0000

    variable,datatypes,conversions,memory allocation

---
+++ basics/01_variables.js
@@ -0,0 +1,83 @@
+// console.log("Yash")
+// const accId = 1234
+// let age = 21
+// var name = "OM"
+// let demo;
+// console.log(age) // 21
+// console.log(demo); //return undefined
+// console.log(3+2); // 5
+
+
+
+
+
+// let n = "Yash";
+// let a = 21
+// let isBool = true
+
+// Datatypes
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+// Object
+
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+
+
+
+
+// let n = "33";
+// console.log(typeof n);
+// let a = Number(n);
+// console.log(typeof(a));
+// The Conversions
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+// console.log("2" == 2); // return true because "2" convert into 2 then compare
+// console.log("2" === 2); // return false because also checks the datatypes
+
+
+
+// Datatypes
+// primitive = there are 7  datatypes(Number, String, Boolean, null, undefined, Symbol, BigInt )
+// non-primitives = array, object, functions (all non-primitive datatype is object)
+// let colors = ["red","blue","green"] // Array
+// let myObj = {
+//     name : "Yash",
+//     age : 21 
+// } // Object
+// const myFunction = function (){
+//     console.log("Hello");
+    
+// }// function
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++
+
+// memory allcation
+
+// Stack (Primitive DataTypes) and Heap(Non-Primitive DataTypes)
+
+// Stack (Primitive DataTypes)
+// let myName = "Yash"
+// let yourName = myName // Yash
+// yourName = "om" // om
+// console.log(myName); // Yash
+// console.log(yourName); // om
+
+// Heap(Non-Primitive DataTypes)
+// let a = {
+//     name : "yash"
+// }
+// let b = a
+// b.name = "om"
+// console.log(a.name); // om
+// console.log(b.name); // om
+
+
