11260782c5fd987c10c165fbdd802d4eacd51506
Yash Adke <122884838+yashgithub2003@users.noreply.github.com>
2026-01-09 13:13:16 +0000

    Strings and Date TIme

---
+++ basics/02.js
@@ -0,0 +1,107 @@
+// Strings in JavaScript
+// let name = 'Yash'
+// let age = 21
+
+// console.log(`My name is ${name} and age is ${age}`);
+
+// let gameName = new String("BGMI")
+// console.log(typeof gameName);
+// console.log(gameName[0]);
+// console.log(gameName);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('G'));
+
+// const subString = gameName.substring(0,2);
+// console.log(subString);
+// const subString2 = gameName.slice(-2,4);
+// console.log(subString2);
+
+// const newStr = "    Welcome.   "
+// console.log(newStr.trim()); // remove both start and end spaces
+// console.log(newStr.trimStart()); // remove only start spaces
+// console.log(newStr.trimEnd()); // remove end spaces
+// console.log(newStr.replace(" ","-")); // replace oly first occurance
+// console.log(newStr.replaceAll(" ","-")); // replace all occurance
+// console.log(newStr.includes("Wel")); // return bool 
+
+// const a = "A B C D";
+// console.log(a.split(" ")); // convert string into array
+
+
+// ---------------------------------------------------------
+// Numbers and Maths in Javascript
+
+// let a = 100 
+// console.log(a.toString().length);
+// console.log(a.toFixed(2));
+
+// const b = 123.2342
+// console.log(b.toPrecision(4));
+// const c = 100000000
+// console.log(c.toLocaleString('en-IN'));
+
+
+// ================ Maths ==========================
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.3));
+// console.log(Math.min(2,3,1,4,5));
+// console.log(Math.max(2,3,1,4,5));
+
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1 );
+// const min = 5
+// const max = 8
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+// ------------ Dates and Time in JavaScript -------------
+
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+// let curDate = new Date(2023,1,18,8,43,32)
+// console.log(curDate.toDateString())
+// console.log(curDate.toLocaleString())
+// let curDate = new Date("02-21-2026")
+// console.log(curDate.toDateString())
+// console.log(curDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+// let newDate = new Date();
+// console.log(newDate.getDate());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
